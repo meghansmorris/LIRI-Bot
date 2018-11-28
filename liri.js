6baa69114db8367cccdd33f8d3d2111ebfdac4bc
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-11-28 14:53:45 -0700

    installing npm apps

--- liri.js
+++ liri.js
@@ -1,3 +1,9 @@
 //code to read and set any environment variables with the dotenv package
 require("dotenv").config(); 
 
+var keyInfo = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+console.log(keyInfo);
+
